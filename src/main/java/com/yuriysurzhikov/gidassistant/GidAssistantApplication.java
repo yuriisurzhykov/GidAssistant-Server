f1483d8ec82ab9eb69877562a89165076c6dc27b
yurii.surzhykov <yurii.surzhykov@gcpsoftware.com>
2020-10-26 22:20:38 +0200

    make it better

---
+++ src/main/java/com/yuriysurzhikov/gidassistant/GidAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.yuriysurzhikov.gidassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GidAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GidAssistantApplication.class, args);
+    }
+
+}
