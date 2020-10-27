2967621fa655b6a7608210b82bee27e4fcbcc16c
yuriysurzhikov <yuriy.surzhickov@yandex.ua>
2020-10-27 23:24:31 +0200

    Change main entry point greetings
    (cherry picked from commit 61ea69ef5f67683a9cbefc07db7622fc01f4ba5c)

--- src/main/java/com/yuriysurzhikov/gidassistant/controllers/MainController.java
+++ src/main/java/com/yuriysurzhikov/gidassistant/controllers/MainController.java
@@ -1,7 +1,13 @@
 package com.yuriysurzhikov.gidassistant.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+
+    @GetMapping("/")
+    public String getGreetings() {
+        return "Hello, dear friend!";
+    }
 }
